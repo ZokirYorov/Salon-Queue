b2303d97d60d7b81ee3c1bc34315fbca3ade6868
zokir yorov <user_upg@gmail.com>
2026-07-31 09:51:55 +0500

    some changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import {defineConfig, loadEnv} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { resolve } from 'path'
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '')
@@ -14,21 +14,21 @@ export default defineConfig(({ mode }) => {
 
         resolve: {
             alias: {
-                "@": path.resolve(__dirname, "src"),
+                "@": resolve(__dirname, "src"),
             }
         },
-        // server: {
-        //     port: 5175,
-        //     proxy: {
-        //         '/api': {
-        //             target: env.VITE_BASE_API || 'http://34.76.229.135',
-        //             changeOrigin: true,
-        //         },
-        //         '/uploads': {
-        //             target: env.VITE_BASE_API || 'http://34.76.229.135',
-        //             changeOrigin: true,
-        //         },
-        //     }
-        // }
+        server: {
+            port: 5175,
+            proxy: {
+                '/api': {
+                    target: env.VITE_BASE_API,
+                    changeOrigin: true,
+                },
+                '/uploads': {
+                    target: env.VITE_BASE_API,
+                    changeOrigin: true,
+                },
+            }
+        }
     }
 })
