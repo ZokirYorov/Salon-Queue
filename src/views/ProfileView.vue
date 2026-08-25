b101f5a11bce361b85e23c1616ffc707db47d26c
zokir yorov <user_upg@gmail.com>
2026-08-25 14:43:59 +0500

    profile page business link added

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -48,9 +48,18 @@
               <RouterLink to="/client/my" class="rounded-xl border border-slate-200 px-3 py-2 text-xs font-black text-slate-600 transition hover:border-teal-400 hover:text-teal-700 dark:border-slate-600 dark:text-slate-300 dark:hover:border-teal-400 dark:hover:text-teal-300">
                 Navbatlarim
               </RouterLink>
-              <button type="button" @click="openBusinessApp" class="rounded-xl cursor-pointer bg-teal-600 px-3 py-2 text-xs font-black text-white transition hover:bg-teal-700">
+              <a
+                  class="rounded-xl cursor-pointer bg-teal-600 px-3 py-2 text-xs font-black text-white transition hover:bg-teal-700"
+                  href="https://onetime-managment.netlify.app"
+              >
                 Biznes yaratish
-              </button>
+              </a>
+<!--              <button type="button"-->
+<!--                      @click="openBusinessApp" -->
+<!--                      class="rounded-xl cursor-pointer bg-teal-600 px-3 py-2 text-xs font-black text-white transition hover:bg-teal-700"-->
+<!--              >-->
+<!--                Biznes yaratish-->
+<!--              </button>-->
             </div>
           </div>
 
@@ -190,9 +199,9 @@ async function loadProfile() {
   applyProfile(data);
 }
 
-function openBusinessApp() {
+// function openBusinessApp() {
   // window.location.href = businessAppUrl;
-}
+// }
 
 onMounted(async () => {
   try {
